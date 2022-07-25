8b47a40574d76a06b74ee404f607838fb124f3ba
hhhxiao <1417599491@qq.com>
2022-07-26 02:21:18 +0800

    Add spawn simulator

--- js/index.js
+++ js/index.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
     $('#rng-btn').click(function () {
         window.location.href = "./rng.html";
     });
+    $('#spawn-btn').click(function () {
+        window.location.href = "./spawn.html";
+    });
 });
